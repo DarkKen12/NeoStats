125fb0d1c32ec431e74aefe665c81821e8d13ee1
DarkKen12 <103071412+DarkKen12@users.noreply.github.com>
2023-04-28 20:49:28 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,6 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg ">
     <div className="container-fluid">
       <a className="navbar-brand" href="/">NeoStats</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
     </div>
   </nav>
   )
